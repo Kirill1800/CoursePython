0c536015ab4d5b10fb435932130de6b985d87ab9
Kirill1800 <kirill.glushakov03@mail.ru>
2020-11-26 20:59:46 +0300

    Вызвал ошибку

---
+++ Lesson02/L2(HW).py
@@ -0,0 +1,23 @@
+# - вызываем ошибки
+print("\nError 1")
+try:
+    print(int("Привет мир!"))
+except ValueError:
+    print("Была ошибка")
+
+print("\nError 2")
+print()
+try:
+    print(a)
+except NameError:
+    print("Была ошибка")
+
+print("\nError 3")
+try:
+    a = 8
+    b = 10
+    c = a * b
+    print(c)
+except SyntaxError:
+    print("Была ошибка")
+print("\nError 4")
