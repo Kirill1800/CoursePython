b8be34be3467900a8bab0f11f1867f6301b59a2e
Igor <yudin-igor@rambler.ru>
2020-12-21 22:11:16 +0300

    Урок 6

--- Lesson06/L6(CW).py
+++ Lesson06/L6(CW).py
@@ -1,28 +1,45 @@
-class Color:
+# Класс (структура, один)
+# Объект (несколько)
+
+# Класс RGB
+class Color:  # Объявление нашего класса (создание)
+
+    # Параметры класса Color
     red = 0
     green = 0
     blue = 0
 
-    def _init_(self, r, g, b):
-        red = r
-        green = g
-        blue = b
+    # Инициализация класса (выполняется всегда при создании объекта)
+    def __init__(self, r, g, b):
+        self.red = r
+        self.green = g
+        self.blue = b
 
-    def toHex(self):
-        return '#%02x%02x%02x' % (red, green, blue)
+    # Метод(Функция) №1 класса Color (который переводит RGB переводит в 16bit)
+    def to_hex(self):
+        return '#%02x%02x%02x' % (self.red, self.green, self.blue)  # Штука которая из RGB переводит в 16bit
+
+    # Метод(Функция) №2 класса Color (которая сумирует три цвета в числовом виде)
+    def sum_rgb(self):
+        result = self.red + self.green + self.blue
+        return result
 
 
 class ColorAlpha(Color):
     alpha = 1
 
-    def _init_(self, r, g, b, a):
-        red = r
-        green = g
-        blue = b
-        alpha = a
+    def __init__(self, r, g, b, a):
+        self.red = r
+        self.green = g
+        self.blue = b
+        self.alpha = a
 
 
-gray = Color(80, 80, 80)
+gray_2 = Color(100, 100, 100)
 
-red = ColorAlpha(255, 0, 0, .5)
+blue = 40
+red = ColorAlpha(255, 0, 0, 0.5)  # Создаем объект red
+print(type(blue), type(red))  # Печатаем типы данных переменных red и blue
+print(red.to_hex())  # Печатем результат выполнения метода to.hex() для объекта red
+print(red.sum_rgb())  # Печатем результат выполнения метода sum_rgb() для объекта red
 
