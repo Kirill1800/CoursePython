7a2cb1ee51564ebf6fa6917ede5a6ac479504fc0
Kirill1800 <kirill.glushakov03@mail.ru>
2020-12-30 22:48:20 +0300

    Домашняя работа №7

--- Lesson07/L7(HW).py
+++ Lesson07/L7(HW).py
@@ -1,5 +1,6 @@
 #  from flask import Flask
 import requests
+import json
 
 from time import sleep
 
@@ -12,9 +13,34 @@ URL = "https://api.telegram.org/bot1420906671:AAFYtk31Zd2Ftyqc58S4hJVJFzL0FO3TVU
 def get_updates():
     url = URL + 'getupdates'
     r = requests.get(url)
-    print(r.content)
+    return r.json()
+
+
+def get_message():
+    data = get_updates()
+
+    chat_id = data['result'][-1]['message']['chat']['id']
+    text = data['result'][-1]['message']['text']
+    print(text)
+    #  result = {'chat_id': chat_id, 'text': text}
+    #  return result
+
+
+def main():
+    d = get_updates()
+    get_message()
+
+    with open('updates.json', 'w') as file:
+        json.dump(d, file, indent=2, ensure_ascii=False)
+
+
+def write_json(data, file_name='answer.json'):
+    with open(file_name, 'w') as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
 
 
 def main():
     r = requests.get(URL + 'getMe')
-    print(r.json())
+    write_json(r.json())
+
+#  if __name__ == '__maim__':
