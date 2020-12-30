423d750b943d93f8a223fbd3f7a388f5f5eee853
Igor <yudin-igor@rambler.ru>
2020-12-30 23:26:32 +0300

    Измененния

--- Lesson07/L7(HW).py
+++ Lesson07/L7(HW).py
@@ -17,14 +17,14 @@ def get_updates():
     return r.json()
 
 
+# Получает последнее сообщение
 def get_message():
     data = get_updates()
 
     chat_id = data['result'][-1]['message']['chat']['id']
     text = data['result'][-1]['message']['text']
-    print(text)
-    #  result = {'chat_id': chat_id, 'text': text}
-    #  return result
+    result = {'chat_id': chat_id, 'text': text}
+    return result
 
 
 def write_json(data, file_name):
@@ -32,19 +32,14 @@ def write_json(data, file_name):
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
-def my_global():
-    # 1. Тут мы получили информацию о боте и записали ее в файл "getMe.json"
-    r = requests.get(URL + 'getMe')
-    write_json(data=r.json(), file_name='getMe.json')
+# 1. Тут мы получили информацию о боте и записали ее в файл "getMe.json"
+r = requests.get(URL + 'getMe')
+write_json(data=r.json(), file_name='getMe.json')
 
-    # 2. Записываем все сообщения в файл "messages.json"
-    result_get_updates = get_updates()
-    write_json(data=result_get_updates, file_name="messages.json")
+# 2. Записываем все сообщения в файл "messages.json"
+result_get_updates = get_updates()
+write_json(data=result_get_updates, file_name="messages.json")
 
-    # 3.
-    get_message()
-
-
-# Запуск главной функции для Бота
-if __name__ == '__main__':
-    my_global()
+# 3. Получаем последнее сообщение и записываем его в "last_message.json"
+result_get_message = get_message()
+write_json(data=result_get_message, file_name="last_message.json")
