8ba01a5ff82c1903f732fdb7fca9ce0edea393cf
Kirill1800 <kirill.glushakov03@mail.ru>
2021-02-11 23:32:34 +0300

    сайт дз

---
+++ Lesson 12/L12(CW).py
@@ -0,0 +1,26 @@
+import pymongo
+
+string = "mongodb+srv://root:root@cluster0.sygbz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(string)
+collection = client["MyDataBase"]["Test1"]
+
+# Сохранение (добавление)
+data = {"Name": ["Кирил", "Игорь"]}
+collection.save(data)
+
+# Получение (простое)
+result = collection.find()[0]
+print(result)
+
+# Получение (сложное)
+for line in collection.find():
+    if line['Name'] == ["Кирил", "Игорь"]:
+        result = line
+print(result)
+
+# Удаление
+collection.delete_many({"Name": ["Кирил", "Игорь"]})
+
+# Обновление данных
+collection.update_many({"Name": ["Кирилл", "Игорь"]}, {"$set": {"Name": ["Кирилл", "Игорь", "Аня"]}})
