44b428521788f6f0e5efaf514ce3e6a8a0324e8a
Igor <yudin-igor@rambler.ru>
2021-02-20 20:09:53 +0300

    фото

--- resume/main.py
+++ resume/main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, url_for
 import pymongo
+import ssl
 
-string = "mongodb+srv://root:root@cluster0.sygbz.mongodb.net/<dbname>?retryWrites=true&w=majority"
-client = pymongo.MongoClient(string)
-collection = client["MyDataBase"]["Test2"]
-finish = collection.find()[0]
+
+# collection = client["MyDataBase"]["Test2"]
+# finish = collection.find()[0]
 
 app = Flask(__name__)
 
+# Подключение к нашей БД
+string = "mongodb+srv://root:root@cluster0.sygbz.mongodb.net/<dbname>?retryWrites=true&w=majority"
+client = pymongo.MongoClient(string, ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
+
+# Основной код
 @app.route('/')
 def index():
+
+    # Формирование главной HTML нашего резюме
     title = "Мое резюме"
     return render_template("index.html", TITLE=title, SKILL=skill_development(),
                            SKILL_TITLE=skill_development(mode="Title"), SOFT=new_skills(),
@@ -42,10 +49,9 @@ def new_skills(mode=None):
 
 def experience(mode=None):
     if not mode:
-        result = ["Freelance Web Designer/Developer ~ 2007-2009", "Best Buy - Geek Squad In-Store Agent ~ 2008-2009",
-                  "Freelance Writer for Hongkiat.com ~ 2011-Present"]
+        result = client["MyDataBase"]["Information"].find({"Name": "Опыт-Тело"})[0]["result"]
     elif mode == "Title":
-        result = "Опыт работы"
+        result = client["MyDataBase"]["Information"].find({"Name": "Опыт-Заголовок"})[0]["result"]
     else:
         result = "Ошибка"
     return result
@@ -53,9 +59,9 @@ def experience(mode=None):
 
 def information(mode=None):
     if not mode:
-        result = collection
+        result = client["MyDataBase"]["Information"].find({"Name": "Информация-Тело"})[0]["result"]
     elif mode == "Title":
-        result = "Немного обо мне"
+        result = client["MyDataBase"]["Information"].find({"Name": "Информация-Заголовок"})[0]["result"]
     else:
         result = "Ошибка"
     return result
@@ -64,3 +70,6 @@ def information(mode=None):
 # Запуск
 if __name__ == '__main__':
     app.run()
+
+# База данных (включает в себя КОЛЛЕКЦИИ)
+# Коллекции (включают в себя СТРОКИ)
